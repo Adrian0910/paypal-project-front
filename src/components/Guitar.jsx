861e4670baf275b6c0baf94fde2a462c8b3fe4f8
Adrian Ibarra <35971687+Adrian0910@users.noreply.github.com>
2024-11-15 18:28:47 -0600

    c
    c

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -17,7 +17,7 @@ const Guitar = ({ guitar, addToCart }) => {
                 type="button"
                 className="btn btn-dark w-100"
                 onClick={() => addToCart(guitar)}
-            >Agregar al Carrito</button>
+            >Agregar al Carrito </button>
         </div>
     </div>
   )
