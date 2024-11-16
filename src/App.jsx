e1543e4eb8a4533c3756948dcd34d67a25b5cb71
Adrian Ibarra <35971687+Adrian0910@users.noreply.github.com>
2024-11-15 18:38:55 -0600

    c
    c

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   } = useCart()
 
   return (
-    <>
+    <Router>
     <Header 
       cart={cart}
       removeFromCart={removeFromCart}
@@ -45,11 +45,11 @@ function App() {
 
     <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+            <p className="text-white text-center fs-4 mt-4 m-md-0">videojuegos - Todos los derechos Reservados</p>
         </div>
     </footer>
 
-    </>
+    </Router>
   )
 }
 
